61fb787da6781c1b646710dd3acb658a909f1a49
anaghastaff <anaghastaff123@gmail.com>
2024-06-02 19:37:11 +0530

    pagination for customer order, addresses implemented

--- src/app/[countryCode]/page.tsx
+++ src/app/[countryCode]/page.tsx
@@ -2,7 +2,7 @@ import { getRegion } from "lib/data";
 import IndexPageView from "../../pages-sections/landing/page-view"
 import { fetchCart } from "medusa/lib/util/get-cart-from-cookie";
 import { Viewport } from "next";
-import {notFound} from 'next/headers'
+import {notFound}  from "next/navigation"
 export const metadata = {
     title: "Bazaar - Next.js E-commerce Template",
     description: `Bazaar is a React Next.js E-commerce template. Build SEO friendly Online store, delivery app and Multi vendor store`,
@@ -34,12 +34,12 @@ export const metadata = {
     // }
     
     const cart = await fetchCart();
+    console.log("cart type", cart?.type)
     if(!cart){
       notFound()
     }
     return (
-      <>
-        
+      <>        
         <div className="py-12">
           <ul className="flex flex-col gap-x-6">
             <IndexPageView countryCode={countryCode} cart={cart}/>
